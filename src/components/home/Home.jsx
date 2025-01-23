b503bf90b164f7be430e53115467c18e96b2727b
SamGwilliamStudent <gwill34@byu.edu>
2025-01-22 19:36:58 -0700

    Update Home.jsx to use S3 links instead of paths

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,10 +6,24 @@ import TypeIt from "typeit-react";
 import "./Home.css"
 
 const Home = () => {
+    const handleDownload = () => {
+        const pdfUrl = "https://web-portfolio-sam.s3.us-west-2.amazonaws.com/SamGwilliamResume.pdf";
+
+        // Open the PDF in a new tab
+        window.open(pdfUrl, '_blank');
+
+        // Trigger the download (simulating click on the download link)
+        const link = document.createElement('a');
+        link.href = pdfUrl;
+        link.download = "SamGwilliamResume.pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
     return (
         <div className="homePage">
             <div className="infoBox">
-                <img src="src/assets/images/headshot.jpg" alt="Headshot" className="headshot" />
+                <img src="https://web-portfolio-sam.s3.us-west-2.amazonaws.com/headshot.jpg" alt="Headshot" className="headshot" loading="lazy" />
                 <div className="textBox">
                     <TypeIt as="h1"
                         options={{
@@ -29,9 +43,9 @@ const Home = () => {
             </div>
             <div className="resumeDownload">
                 <p className="constructionText" >My website is still under construction, but in the meantime please take a look at my work experience! </p>
-                <a href={"src/assets/documents/SamGwilliamResume.pdf"} download="SamGwilliamResume.pdf">
+                <a href={"https://web-portfolio-sam.s3.us-west-2.amazonaws.com/SamGwilliamResume.pdf"} download="SamGwilliamResume.pdf" target={"_blank"}>
                     <Button className="gradient-button-resume" icon={<SolutionOutlined />} iconPosition="start">
-                        Download My Resumé
+                        View and Download My Resumé
                     </Button>
                 </a>
             </div>
